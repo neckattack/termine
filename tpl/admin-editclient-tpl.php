aa6e6ce2f1b1246a94247f94645e6c2f26aa3b59
neckattack <chris@neckattack.net>
2025-11-22 16:32:34 +0100

    Fix: Hinzufügen-Link mit clear:both in eigener Zeile

--- tpl/admin-editclient-tpl.php
+++ tpl/admin-editclient-tpl.php
@@ -236,7 +236,9 @@ require ROOT."/tpl/header-tpl.php";
 				<div class="row row-date-section" style="position: relative;">
 					<label>Tage</label>
 					<div class="dates">
-						<a href="#" id="addDate" style="display: block; margin-bottom: 10px; font-weight: bold;">+ Hinzufügen</a>
+						<div style="clear: both; width: 100%; margin-bottom: 10px;">
+							<a href="#" id="addDate" style="display: block; font-weight: bold;">+ Hinzufügen</a>
+						</div>
 						<?php foreach ($cDays AS $day) {?>
 						<div id="date_<?=$day["id"]?>" class="date-entry" data-date="<?=isset($day["date"]) ? $day["date"] : ""?>">
 							<input type="text" class="datePicker" name="cDays[]" value="<?=isset($day["date"]) ? $day["date"] : ""?>" />
